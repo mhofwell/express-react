c7b645b18b7f0ce7da3110428e1240045be604fd
Michael Hofweller <michael.hofweller@gmail.com>
2021-02-10 21:16:58 -0800

    mongodb schema setup

---
+++ mern-exercise-tracker/backend/models/exercise.models.js
@@ -0,0 +1,27 @@
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+
+const exerciseSchema = new Schema({
+    username: {
+        type: String, 
+        required: true, 
+    },
+    description: {
+        type: String, 
+        required: true, 
+    },
+    duration: {
+        type: Number,
+        required: true, 
+    }, 
+    date: {
+        type: Date, 
+        required: true, 
+    }
+}, {
+    timestamps: true, 
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise; 
