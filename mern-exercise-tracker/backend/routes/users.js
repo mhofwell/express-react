ea2005a1295e7bec52ab76c33465a1e836f71895
Michael Hofweller <michael.hofweller@gmail.com>
2021-02-13 12:08:17 -0800

    tested api and built routes for frontend React application

--- mern-exercise-tracker/backend/routes/users.js
+++ mern-exercise-tracker/backend/routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router(); 
-let User = require('../models/user.model'); 
+let User = require('../models/user.models'); 
 
 router.route('/').get((req, res) => {
     User.find()
--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/navbar.component";
+import ExerciseList from "./components/exercise-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={ExerciseList} />
+        <Route path="/edit/:id" exact component={EditExercise} />
+        <Route path="/create" exact component={CreateExercise} />
+        <Route path="/user" exact component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
